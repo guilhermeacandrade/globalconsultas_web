05811f5c6024d39ca79d0128368afd9bca2f6c37
Guilherme Andrade <guilherme.acandrade@gmail.com>
2024-12-03 02:01:22 -0300

    chore: initial settings

---
+++ src/app/fonts/GoogleFonts/index.tsx
@@ -0,0 +1,24 @@
+import { Inter } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+import { DM_Sans } from "next/font/google";
+
+export const dm_sans = DM_Sans({
+  subsets: ["latin"],
+  weight: [
+    "100",
+    "200",
+    "300",
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+    "900",
+    "1000",
+  ],
+});
