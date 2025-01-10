941366db6078e4b50358c05f5653f839803b97da
Guilherme <guilherme.acandrade@gmail.com>
2025-01-10 12:59:17 -0300

    feat: add toast and configs.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { inter } from "@/app/fonts/GoogleFonts";
 import "./globals.css";
 import SessionProvider from "@/providers/session";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "Global Consultas",
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={`${inter.className}  antialiased`}>
+        <Toaster />
         <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,21 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+export const durationToast: number = 2000;
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export function dateStringToDateUTC(dateStr: string) {
+  const parts = dateStr.split("-");
+  const dateUTC = new Date(
+    Date.UTC(Number(parts[0]), Number(parts[1]) - 1, Number(parts[2]))
+  );
+
+  return dateUTC;
 }
