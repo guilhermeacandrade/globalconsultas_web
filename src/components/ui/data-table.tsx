74f5ac731285f00b11afcc71ab99f98f6ad87a89
Guilherme <guilherme.acandrade@gmail.com>
2025-01-10 23:50:02 -0300

    chore: add component data-table no projeto

---
+++ src/components/ui/data-table.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import * as React from "react";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  VisibilityState,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { cn } from "@/lib/utils";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+  visibilityColumns?: VisibilityState;
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+  visibilityColumns = {},
+}: DataTableProps<TData, TValue>) {
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>(visibilityColumns);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    state: {
+      columnVisibility,
+    },
+  });
+
+  return (
+    <div className="rounded-md border">
+      {/* teste botões -> ativa e desativa visibilidade da coluna */}
+      {/* {table
+        .getAllColumns()
+        .filter((column) => column.getCanHide())
+        .map((column) => {
+          return (
+            <button
+              key={column.id}
+              className="mx-2 capitalize"
+              // checked={column.getIsVisible()}
+              // onCheckedChange={(value) => column.toggleVisibility(!!value)}
+              onClick={() => column.toggleVisibility(!column.getIsVisible())}
+            >
+              {column.id}
+            </button>
+          );
+        })} */}
+
+      <Table>
+        <TableHeader className="bg-muted">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead
+                    key={header.id}
+                    className={cn(
+                      "first:rounded-tl-md last:rounded-tr-md",
+                      header.column.columnDef.meta?.headerClassName
+                    )}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell
+                    key={cell.id}
+                    className={cn(
+                      "",
+                      cell.column.columnDef.meta?.cellClassName
+                    )}
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                Nenhum resultado.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
