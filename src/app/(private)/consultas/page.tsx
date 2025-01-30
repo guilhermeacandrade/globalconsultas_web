e1d0dcc8811c1cb273cfc633b98ffa66fc89a1d9
Guilherme <guilherme.acandrade@gmail.com>
2025-01-30 11:53:07 -0300

    teste pagina de consultas 2

--- src/app/(private)/consultas/page.tsx
+++ src/app/(private)/consultas/page.tsx
@@ -7,7 +7,7 @@ import { IInquiry } from "@/utils/types/inquiry.type";
 import { auth } from "@/lib/auth";
 
 export default async function InquiriesPage() {
-  // const session = await auth();
+  const session = await auth();
 
   // let url: string = "/inquiry";
 
@@ -19,33 +19,34 @@ export default async function InquiriesPage() {
   //   url = url + `/branch/${session.user.branchId}`;
 
   // const resp = await api.get(url);
-  // const records: IInquiry[] = resp.data.data;
-
-  return (
-    <div>
-      <h1>Consultas teste</h1>
-    </div>
-  );
+  const resp = await api.get("/inquiry");
+  const records: IInquiry[] = resp.data.data;
 
   // return (
-  //   <div className="mt-3 px-2 max-w-5xl mx-auto">
-  //     <div className="py-2 flex justify-between items-center">
-  //       <h3 className="text-xl font-semibold">Consultas</h3>
-
-  //       <DialogInquiry
-  //         trigger={
-  //           <button className="flex items-center gap-1 bg-primary text-background py-2 px-3 rounded-lg text-sm hover:bg-primary/90">
-  //             <PlusCircle size={20} />
-  //             Nova Consulta
-  //           </button>
-  //         }
-  //         dialogTitle="Nova Empresa"
-  //       />
-  //     </div>
-
-  //     <div className="my-4">
-  //       <TableInquiry inquiry={records} />
-  //     </div>
+  //   <div>
+  //     <h1>Consultas teste</h1>
   //   </div>
   // );
+
+  return (
+    <div className="mt-3 px-2 max-w-5xl mx-auto">
+      <div className="py-2 flex justify-between items-center">
+        <h3 className="text-xl font-semibold">Consultas</h3>
+
+        <DialogInquiry
+          trigger={
+            <button className="flex items-center gap-1 bg-primary text-background py-2 px-3 rounded-lg text-sm hover:bg-primary/90">
+              <PlusCircle size={20} />
+              Nova Consulta
+            </button>
+          }
+          dialogTitle="Nova Empresa"
+        />
+      </div>
+
+      <div className="my-4">
+        <TableInquiry inquiry={records} />
+      </div>
+    </div>
+  );
 }
